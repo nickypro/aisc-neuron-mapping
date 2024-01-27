c935966e5ff4baf8a973b055096ae7ea0aea71cb
TetraspaceW <holomanga@gmail.com>
2024-01-27 10:32:37 +0000

    test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+from taker.data_classes import PruningConfig
+from taker.parser import cli_parser
+from taker.prune import run_pruning
+import torch
+
+# Configure initial model and tests
+c = PruningConfig(
+    wandb_project="testing",  # repo to push results to
+    model_repo="nickypro/tinyllama-15M",
+    token_limit=1000,  # trim the input to this max length
+    run_pre_test=True,  # evaluate the unpruned model
+    collection_sample_size=1000,
+    eval_sample_size=250,  # number of tokens to evaluate on
+    # Removals parameters
+    ff_frac=0.1,  # % of feed forward neurons to prune
+    attn_frac=0.00,  # % of attention neurons to prune
+    focus="pile",  # the “reference” dataset
+    cripple="bio",  # the “unlearned” dataset
+    additional_datasets=tuple(),  # any extra datasets to evaluate on
+    recalculate_activations=False,  # iterative vs non-iterative
+)
+
+# Parse CLI for arguments
+# c, args = cli_parser(c)
+
+# Run the iterated pruning
+with torch.no_grad():
+    model, history = run_pruning(c)
