14775a9aa80f2c2df02e32ed32e744fa14b8570a
TetraspaceW <holomanga@gmail.com>
2024-02-05 05:29:48 +0000

    use pruning config params from rashid's project

--- image_model_test.py
+++ image_model_test.py
@@ -5,18 +5,19 @@ import torch
 
 pruningConfig = PruningConfig(
     model_repo="Ahmed9275/Vit-Cifar100",
-    run_pre_test=False,  # evaluate the unpruned model
-
-    ff_frac=0.1,  # % of feed forward neurons to prune
-    attn_frac=0.00,  # % of attention neurons to prune
-    focus="cifar20-split",  # the “reference” dataset
-    cripple="cifar20-veh1",  # the “unlearned” dataset
-    additional_datasets=tuple(),  # any extra datasets to evaluate on
-    recalculate_activations=False,  # iterative vs non-iterative
-    eval_sample_size=100,  # number of tokens to evaluate on
-    collection_sample_size=100,  # number of tokens to collect activations on
-    save=True,
-    n_steps=20
+    token_limit  = 1000,  # trim the input to this max length
+    run_pre_test = True,  # evaluate the unpruned model
+    eval_sample_size = 1e2,
+    collection_sample_size = 1e2,
+    # Removals parameters
+    ff_frac   = 0.2,     # % of feed forward neurons to prune
+    attn_frac = 0.00,     # % of attention neurons to prune
+    focus     = "cifar20-split", # the “reference” dataset
+    cripple   = "cifar20-veh1",          # the “unlearned” dataset
+    additional_datasets=tuple(), # any extra datasets to evaluate on
+    recalculate_activations = False, # iterative vs non-iterative
+    n_steps = 1,
+    wandb_project = "testing-tetra-image"
 )
 
 with torch.no_grad():
